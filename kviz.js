424ecee37aed389db21861d219a7e9e4eee58580
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-09 14:41:50 +0200

    stránkování otázek

--- kviz.js
+++ kviz.js
@@ -19,20 +19,40 @@ let kviz = [
   }
 ];
 
-let poradi = document.getElementById("poradi");
-let otazka = document.getElementById("otazka");
-let obrazek = document.getElementById("obrazek");
 let element = null;
 let zvoleneOdpovedi = [];
 let varianty = [];
 
-let i = 0
-while (i <= kviz.length - 1) {
-  element = kviz[i];
-  poradi.innerHTML = `Otázka ${i + 1} / ${kviz.length}`;
-  otazka.innerHTML = element.otazka;
+i = 0
+element = kviz[i];
+vykresliOtazku(i, kviz, element);
+aktivujVarianty(i, kviz, element);
+vykresliVysledky();
+
+function vykresliOtazku(i, kviz, element) {
+  let poradiOtazky = document.createElement('div');
+  poradiOtazky.id = 'poradi';
+  poradiOtazky.textContent = `Otázka ${i + 1} / ${kviz.length}`;
+  document.querySelector('.kviz').appendChild(poradiOtazky);
+  let novaOtazka = document.createElement('h2');
+  novaOtazka.id = 'otazka';
+  novaOtazka.innerHTML = element.otazka;
+  document.querySelector('.kviz').appendChild(novaOtazka);
+  let moznosti = document.createElement('div');
+  moznosti.id = 'moznosti';
+  moznosti.className = 'obsah';
+  document.querySelector('.kviz').appendChild(moznosti);
+  let foto = document.createElement('div');
+  foto.className = 'foto';
+  document.getElementById('moznosti').appendChild(foto);
+  let obrazek = document.createElement('img');
+  obrazek.id = 'obrazek';
+  obrazek.alt = "obrázek";
   obrazek.src = element.obrazek;
-  console.log(otazka.innerHTML);
+  document.querySelector('.foto').appendChild(obrazek);
+  let odpovedi = document.createElement('ul');
+  odpovedi.id = 'odpovedi';
+  document.getElementById('moznosti').appendChild(odpovedi);
   for (n = 0; n <= element.odpovedi.length - 1; n++) {
     let odpoved = element.odpovedi[n];
     let novaPolozka = document.createElement('li');
@@ -41,14 +61,27 @@ while (i <= kviz.length - 1) {
     document.getElementById("odpovedi").appendChild(novaPolozka);
     console.log(odpoved);
   };
+}
+
+function aktivujVarianty(i, kviz, element) {
   varianty = document.querySelectorAll('li');
   varianty.forEach((varianta) => {
     varianta.addEventListener('click', (udalost) => {
       let zvolenaOdpoved = udalost.target.dataset.odpoved;
       zvoleneOdpovedi.push(zvolenaOdpoved);
+      document.querySelector('.kviz').removeChild(document.getElementById('poradi'));
+      document.querySelector('.kviz').removeChild(document.getElementById('otazka'));
+      document.querySelector('.kviz').removeChild(document.getElementById('moznosti'));
+      if (i < kviz.length - 1) {
+        i++;
+        element = kviz[i];
+        vykresliOtazku(i, kviz, element);
+        aktivujVarianty(i, kviz, element);
+      };
     });
-  i++;
   });
-  
 }
 
+function vykresliVysledky (zvoleneOdpovedi, kviz) {
+  
+}
