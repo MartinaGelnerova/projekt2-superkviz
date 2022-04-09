987c6ffe844919e845483c949e566307237f3182
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-09 15:49:34 +0200

    stránka s vyhodnocením kvízu

--- kviz.js
+++ kviz.js
@@ -24,10 +24,13 @@ let zvoleneOdpovedi = [];
 let varianty = [];
 
 i = 0
+let kvizStranka = document.createElement('div');
+kvizStranka.className = 'kviz';
+document.querySelector('body').appendChild(kvizStranka);
+
 element = kviz[i];
 vykresliOtazku(i, kviz, element);
 aktivujVarianty(i, kviz, element);
-vykresliVysledky();
 
 function vykresliOtazku(i, kviz, element) {
   let poradiOtazky = document.createElement('div');
@@ -77,11 +80,41 @@ function aktivujVarianty(i, kviz, element) {
         element = kviz[i];
         vykresliOtazku(i, kviz, element);
         aktivujVarianty(i, kviz, element);
+      } else {
+        vykresliVysledky(zvoleneOdpovedi, kviz);
       };
     });
   });
 }
 
-function vykresliVysledky (zvoleneOdpovedi, kviz) {
-  
+function vykresliVysledky(zvoleneOdpovedi, kviz) {
+  let spravnePocet = 0;
+  let nadpis = document.createElement('h2');
+  nadpis.id = 'poradi';
+  nadpis.textContent = `Tvoje hodnocení`;
+  document.querySelector('.kviz').appendChild(nadpis);
+  for (i = 0; i <= kviz.length -1; i++) {
+    element = kviz[i];
+    let otazka = document.createElement('h2');
+    otazka.id = 'otazka';
+    otazka.innerHTML = `${i+1}. ${element.otazka}`;
+    document.querySelector('.kviz').appendChild(otazka);
+    let odpoved = document.createElement('p');
+    odpoved.className = 'odpovedi';
+    odpoved.innerHTML = `Tvoje odpověď: ${element.odpovedi[zvoleneOdpovedi[i]]}`;
+    document.querySelector('.kviz').appendChild(odpoved);
+    let spravne = document.createElement('p');
+    spravne.className = 'odpovedi';
+    if (zvoleneOdpovedi[i] == element.spravna) {
+      spravne.innerHTML = `To je SPRÁVNĚ.`;
+      spravnePocet++;
+    } else {
+      spravne.innerHTML = `Správná odpověd: ${element.odpovedi[element.spravna]}`;
+    }
+    document.querySelector('.kviz').appendChild(spravne);
+  }
+  let vysledek = document.createElement('h2');
+  vysledek.id = 'vysledek';
+  vysledek.textContent = `Správně ${spravnePocet} ze ${kviz.length} otázek. Úspěšnost ${Math.round(spravnePocet/kviz.length*100)}%.`;
+  document.querySelector('.kviz').appendChild(vysledek); 
 }
